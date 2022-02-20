06c9b42e0b2d12821b7d22c32b5fc08d9abd89db
Leon Si <contact@leonzalion.com>
2022-02-20 13:48:12 -0500

    feat: bundle cjs

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,7 @@
 import { execaCommandSync as exec } from 'execa';
-import { copyPackageFiles } from 'lion-system';
+import { copyPackageFiles, chProjectDir } from 'lion-system';
 
-exec('tsc');
+chProjectDir(import.meta.url);
+exec('tsup src/index.ts --format cjs,esm');
 exec('tsc-alias');
 copyPackageFiles();
