47c05df6e0b787f1c3d7620c92f64676ca54e6f6
Leon Si <contact@leonzalion.com>
2022-05-27 11:32:51 -0400

    fix: bug

--- src/prompt.ts
+++ src/prompt.ts
@@ -1,142 +1,144 @@
 /* eslint-disable @typescript-eslint/no-unnecessary-qualifier */
 
-import type inquirer from 'inquirer';
-import type { Answers, Question } from 'inquirer';
-import InputPromptBase from 'inquirer/lib/prompts/base.js';
-import observe from 'inquirer/lib/utils/events.js';
-import type { Interface } from 'node:readline';
-import ora from 'ora';
+import type inquirer from "inquirer";
+import type { Answers, Question } from "inquirer";
+import InputPromptBase from "inquirer/lib/prompts/base.js";
+import observe from "inquirer/lib/utils/events.js";
+import type { Interface } from "node:readline";
+import ora from "ora";
 
-import type { KeyDescriptor } from '~/types.js';
+import type { KeyDescriptor } from "~/types.js";
 
-declare module 'inquirer' {
-	interface PressToContinuePromptOptions<
-		T extends inquirer.Answers = inquirer.Answers
-	> extends inquirer.InputQuestionOptions<T> {}
+declare module "inquirer" {
+  interface PressToContinuePromptOptions<
+    T extends inquirer.Answers = inquirer.Answers
+  > extends inquirer.InputQuestionOptions<T> {}
 
-	/**
-	 * Provides options for the `PressToContinuePrompt`.
-	 *
-	 * @template T
-	 * The type of the answers.
-	 */
+  /**
+   * Provides options for the `PressToContinuePrompt`.
+   *
+   * @template T
+   * The type of the answers.
+   */
 
-	interface PressToContinuePrompt<T extends inquirer.Answers = inquirer.Answers>
-		extends PressToContinuePromptOptions<T> {
-		/**
-		 * @inheritdoc
-		 */
-		type: 'press-to-continue';
+  interface PressToContinuePrompt<T extends inquirer.Answers = inquirer.Answers>
+    extends PressToContinuePromptOptions<T> {
+    /**
+     * @inheritdoc
+     */
+    type: "press-to-continue";
 
-		/**
-		 * The key the user should press.
-		 */
-		key?: string;
+    /**
+     * The key the user should press.
+     */
+    key?: string;
 
-		/**
-		 * Whether to allow any key to be pressed.
-		 */
-		anyKey?: boolean;
+    /**
+     * Whether to allow any key to be pressed.
+     */
+    anyKey?: boolean;
 
-		/**
-		 * Only allow enter to be pressed.
-		 */
-		enter?: boolean;
+    /**
+     * Only allow enter to be pressed.
+     */
+    enter?: boolean;
 
-		/**
-		 * Custom message for the "press to continue" loader
-		 */
-		pressToContinueMessage?: string;
-	}
+    /**
+     * Custom message for the "press to continue" loader
+     */
+    pressToContinueMessage?: string;
+  }
 
-	interface QuestionMap<T extends inquirer.Answers = inquirer.Answers> {
-		/**
-		 * The `PressToContinuePrompt` type.
-		 */
-		pressToContinue: PressToContinuePrompt<T>;
-	}
+  interface QuestionMap<T extends inquirer.Answers = inquirer.Answers> {
+    /**
+     * The `PressToContinuePrompt` type.
+     */
+    pressToContinue: PressToContinuePrompt<T>;
+  }
 }
 
 class PressToContinuePrompt extends InputPromptBase {
-	declare opt: inquirer.prompts.PromptOptions & {
-		key: string;
-		enter: boolean;
-		anyKey: boolean;
-		pressToContinueMessage: string;
-	};
+  declare opt: inquirer.prompts.PromptOptions & {
+    key: string;
+    enter: boolean;
+    anyKey: boolean;
+    pressToContinueMessage: string;
+  };
 
-	spinnerIntervalId: NodeJS.Timeout;
+  spinnerIntervalId: NodeJS.Timeout;
 
-	done: (value: any) => void;
+  done: (value: any) => void;
 
-	constructor(questions: Question, rl: Interface, answers: Answers) {
-		super(questions, rl, answers);
+  constructor(questions: Question, rl: Interface, answers: Answers) {
+    super(questions, rl, answers);
 
-		if (
-			[
-				this.opt.anyKey !== undefined,
-				this.opt.enter !== undefined,
-				this.opt.key !== undefined,
-			].filter((option) => option).length !== 1
-		) {
-			throw new Error(
-				'Exactly one of the options `enter`, `key`, or `anyKey` must be set.'
-			);
-		}
-	}
+    if (
+      [
+        this.opt.anyKey !== undefined,
+        this.opt.enter !== undefined,
+        this.opt.key !== undefined,
+      ].filter((option) => option).length !== 1
+    ) {
+      throw new Error(
+        "Exactly one of the options `enter`, `key`, or `anyKey` must be set."
+      );
+    }
+  }
 
-	_run(done: (value: any) => void) {
-		this.done = done;
+  _run(done: (value: any) => void) {
+    this.done = done;
 
-		const events = observe(this.rl);
+    const events = observe(this.rl);
 
-		events.line.subscribe(() => {
-			this._done({
-				key: {
-					name: 'Enter',
-				},
-				value: 'enter',
-			});
-		});
+    events.line.subscribe(() => {
+      if (this.opt.enter) {
+        this._done({
+          key: {
+            name: "Enter",
+          },
+          value: "enter",
+        });
+      }
+    });
 
-		events.keypress.subscribe((event) => {
-			if (this.opt.anyKey) {
-				this._done(event);
-			} else if (this.opt.enter && event.key.name === 'Enter') {
-				this._done(event);
-			} else if (event.key.name === this.opt.key) {
-				this._done(event);
-			}
-		});
+    events.keypress.subscribe((event) => {
+      if (this.opt.anyKey) {
+        this._done(event);
+      } else if (this.opt.enter && event.key.name === "Enter") {
+        this._done(event);
+      } else if (event.key.name === this.opt.key) {
+        this._done(event);
+      }
+    });
 
-		this.render();
+    this.render();
 
-		return this;
-	}
+    return this;
+  }
 
-	_done(key: KeyDescriptor) {
-		if (this.spinnerIntervalId !== undefined) {
-			clearInterval(this.spinnerIntervalId);
-		}
+  _done(key: KeyDescriptor) {
+    if (this.spinnerIntervalId !== undefined) {
+      clearInterval(this.spinnerIntervalId);
+    }
 
-		this.done(key);
-	}
+    this.done(key);
+  }
 
-	render() {
-		const message =
-			this.opt.pressToContinueMessage ??
-			`Press ${this.opt.enter ? 'enter' : 'any key'} to continue...`;
+  render() {
+    const message =
+      this.opt.pressToContinueMessage ??
+      `Press ${this.opt.enter ? "enter" : "any key"} to continue...`;
 
-		const spinner = ora({
-			// Default interval seems a bit glitchy on Alacritty
-			interval: 100,
-			text: message,
-		});
+    const spinner = ora({
+      // Default interval seems a bit glitchy on Alacritty
+      interval: 100,
+      text: message,
+    });
 
-		this.spinnerIntervalId = setInterval(() => {
-			this.screen.render(spinner.frame(), '');
-		}, spinner.interval);
-	}
+    this.spinnerIntervalId = setInterval(() => {
+      this.screen.render(spinner.frame(), "");
+    }, spinner.interval);
+  }
 }
 
 export default PressToContinuePrompt;
